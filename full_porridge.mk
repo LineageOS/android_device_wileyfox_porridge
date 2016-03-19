d7b27c5e0919204a0a972361aefbb3015a5a5553
Diogo Ferreira <diogo@underdev.org>
2016-03-19 12:27:38 +0000

    porridge: Inherit from core_64_bit
    This triggers intialization of the 64-bit zygote, we were previously
    running the app runtime in 32-bit only which meant that some apps that
    only have 64-bit jni dependencies weren't actually working correctly.
    
    Change-Id: I120eb8854f1e922f24169cba1700ca11342e8373

--- full_porridge.mk
+++ full_porridge.mk
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from porridge
