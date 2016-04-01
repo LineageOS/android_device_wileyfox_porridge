b326d1a74c9cdfd8924b9275aaabada4747f7b57
Ricardo Cerqueira <ricardo@cyngn.com>
2016-04-01 17:15:01 +0100

    telephony: Add WF's custom ECC list
    Change-Id: Id803e7f4dc2e6c87b5c0e8ea27cfe71f4ecb62b9

--- device.mk
+++ device.mk
@@ -56,6 +56,9 @@ PRODUCT_PROPERTY_OVERRIDES += ro.telephony.sim.count=$(SIM_COUNT)
 PRODUCT_PROPERTY_OVERRIDES += persist.radio.default.sim=0
 PRODUCT_PROPERTY_OVERRIDES += persist.radio.multisim.config=dsds
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/config/ecc_list.xml:system/etc/ecc_list.xml
+
 # Graphics
 MTK_GPU_VERSION := mali midgard r7p0
 
