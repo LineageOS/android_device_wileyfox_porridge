a22be0e8af6d87d4ede77236b180675b6a140ab1
Diogo Ferreira <defer@cyngn.com>
2016-04-28 18:47:36 +0100

    porridge: Re-enable compass
    Final samples actually have one.
    
    Change-Id: I78887d558f022c0e6eff479c41288e67e9c43e58

--- device.mk
+++ device.mk
@@ -29,6 +29,7 @@ PRODUCT_COPY_FILES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
