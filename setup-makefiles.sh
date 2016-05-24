1b692b7f85ab000eb5651c4f738cf9ebd551083c
Diogo Ferreira <defer@cyngn.com>
2016-05-24 17:38:09 +0100

    porridge: Add custom audio policy manager
    Change-Id: Ib0bee40ae1896a57969443276a8f5138f5e1d5e9

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -172,6 +172,26 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := platform
 include \$(BUILD_PREBUILT)
 
+include \$(CLEAR_VARS)
+LOCAL_MODULE := libaudiopolicymanager
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MULTILIB := 32
+LOCAL_SRC_FILES_32 := proprietary/lib/libaudiopolicymanager.so
+include \$(BUILD_PREBUILT)
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE := libaudiopolicymanager
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MULTILIB := 64
+LOCAL_SRC_FILES_64 := proprietary/lib64/libaudiopolicymanager.so
+include \$(BUILD_PREBUILT)
+
 endif
 endif
 
